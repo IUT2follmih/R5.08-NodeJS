7b8d2ecc83d60358e276eda060732b404ebd5cf8
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-24 15:43:31 +0200

    Add gray filter

--- TP1/src/models/filters/grayscale.js
+++ TP1/src/models/filters/grayscale.js
@@ -4,12 +4,23 @@
  * @returns the underlying pixel data updated by the filter.
  */
 export const grayscale = function (imageData) {
-  // Extract the pixels only
-  const { data } = imageData;
+    // Extract the pixels only
+    const {data} = imageData;
 
-  // TODO: implement the grayscale filter.
-  
-  return imageData;
+    let longueur = data.length;
+    for (let i = 0; i < longueur; i += 4) {
+        let r = data[i];
+        let g = data[i + 1];
+        let b = data[i + 2];
+
+        let gray = r * 0.299 + g * 0.587 + b * 0.114;
+
+        data[i] = gray
+        data[i + 1] = gray
+        data[i + 2] = gray
+    }
+
+    return imageData;
 };
 
 export default grayscale;
