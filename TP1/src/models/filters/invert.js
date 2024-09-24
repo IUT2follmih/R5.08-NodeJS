288b9a93cf7457c560001d960b560a128d7624e6
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-24 15:45:53 +0200

    Add invert filter

--- TP1/src/models/filters/invert.js
+++ TP1/src/models/filters/invert.js
@@ -4,12 +4,17 @@
  * @returns the underlying pixel data updated by the filter.
  */
 export const invert = function (imageData) {
-  // Extract the pixels only
-  const { data } = imageData;
+    // Extract the pixels only
+    const {data} = imageData;
 
-  // TODO: implement the invert filter.
-  
-  return imageData;
+    let longueur = data.length;
+    for (let i = 0; i < longueur; i += 4) {
+        data[i] = 255 - data[i]
+        data[i + 1] = 255 - data[i + 1]
+        data[i + 2] = 255 - data[i + 2]
+    }
+
+    return imageData;
 };
 
 export default invert;
